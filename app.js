d56a7557f6fb2981f6362e19213aa2c832cfa863
Alexandr Timofeev <144218346+aatutor@users.noreply.github.com>
2024-02-06 18:00:01 +0700

    add-pres

--- app.js
+++ app.js
@@ -5,23 +5,17 @@ var port = 3141
 
 
 app.get(`/`, (request, response) => {
-    var url = request.url;//.slice(1);
-    console.log(url);
-
     fs.readdir('pres', (err, files) => {
         var list = files.filter( (file) => file.endsWith('.html'));
-        // console.log(list);
         var body = '<body><ul>';
         list.forEach( (item) => {
             body += '<li><a href="' + item + '">' + item.slice(0, item.length-5) + '</a></li>\n';
         } );
-        // console.log(body);
         response.send(body);
     });
-    // response.send('hello world');
 })
-
 app.use(express.static(__dirname+'/pres'));
+app.use(express.static(__dirname+'/reveal'));
 app.use(express.static(__dirname+'/files'));
 
 app.listen(port, (err) => {
